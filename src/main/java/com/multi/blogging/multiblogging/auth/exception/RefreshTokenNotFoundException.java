80983c37bb307a60cda59c4abf20fcc353c27b22
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-09-14 22:21:12 +0900

    Cancel RefreshTokenNotFoundException.java

--- src/main/java/com/multi/blogging/multiblogging/auth/exception/RefreshTokenNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.multi.blogging.multiblogging.auth.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND,reason = "토큰을 찾을 수 없습니다.")
-public class RefreshTokenNotFoundException extends RuntimeException{
-}
