c5efc5228206c8d7e07710f4a685780e9f638fd4
goldsergeant <rmagksfla000@gmail.com>
2023-09-16 18:40:29 +0900

    test: mail test success

--- src/main/java/com/multi/blogging/multiblogging/mail/MailService.java
+++ src/main/java/com/multi/blogging/multiblogging/mail/MailService.java
@@ -1,42 +1,36 @@
-//package com.multi.blogging.multiblogging.mail;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Slf4j
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class MailService {
-//
-//    @Value("${spring.mail.username}")
-//    private String mailFromUserName;
-//
-//    private final JavaMailSender mailSender;
-//
-//    public void sendEmail(String toEmail, String title, String text) {
-//        SimpleMailMessage emailForm = createEmailForm(mailFromUserName,toEmail, title, text);
-//        try {
-//            mailSender.send(emailForm);
-//        } catch (RuntimeException e) {
-//            log.debug("MailService.sendEmail exception occur toEmail: {}, " + "title: {}, text: {}", toEmail, title, text);
-//            throw new RuntimeException("메일 발송 실패");
-//        }
-//    }
-//
-//    // 발신할 이메일 데이터 세팅
-//    private SimpleMailMessage createEmailForm(String from,String toEmail, String title, String text) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(toEmail);
-//        message.setSubject(title);
-//        message.setText(text);
-//        message.setFrom(from);
-//
-//        return message;
-//    }
-//}
+package com.multi.blogging.multiblogging.mail;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String title, String text) {
+        SimpleMailMessage emailForm = createEmailForm(toEmail, title, text);
+        try {
+            mailSender.send(emailForm);
+        } catch (RuntimeException e) {
+            log.debug("MailService.sendEmail exception occur toEmail: {}, " + "title: {}, text: {}", toEmail, title, text);
+            throw new RuntimeException("메일 발송 실패");
+        }
+    }
+
+    // 발신할 이메일 데이터 세팅
+    private SimpleMailMessage createEmailForm(String toEmail, String title, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(title);
+        message.setText(text);
+
+        return message;
+    }
+}
--- src/main/java/com/multi/blogging/multiblogging/mail/config/EmailConfig.java
+++ src/main/java/com/multi/blogging/multiblogging/mail/config/EmailConfig.java
@@ -1,69 +1,47 @@
-//package com.multi.blogging.multiblogging.mail.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//
-//import java.util.Properties;
-//
-//@Configuration
-//public class EmailConfig {
-//
-//    @Value("${spring.mail.host}")
-//    private String host;
-//
-//    @Value("${spring.mail.port}")
-//    private int port;
-//
-//    @Value("${spring.mail.username}")
-//    private String username;
-//
-//    @Value("${spring.mail.password}")
-//    private String password;
-//
-//    @Value("${spring.mail.properties.mail.smtp.auth}")
-//    private boolean auth;
-//
-//    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
-//    private boolean starttls;
-//
-//    @Value("${spring.mail.properties.mail.smtp.timeout}")
-//    private int timeout;
-//
-//    @Value("${spring.mail.properties.mail.smtp.ssl.enable}")
-//    private boolean sslEnable;
-//
-//    @Value("${spring.mail.properties.mail.debug}")
-//    private boolean debugEnable;
-//
-//    @Bean
-//    public JavaMailSender javaMailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost(host);
-//        mailSender.setPort(port);
-//        mailSender.setUsername(username);
-//        mailSender.setPassword(password);
-//        mailSender.setDefaultEncoding("UTF-8");
-//        mailSender.setJavaMailProperties(getMailProperties());
-//
-//        return mailSender;
-//    }
-//
-//    private Properties getMailProperties() {
-//        Properties properties = new Properties();
-//        properties.put("mail.smtp.auth", auth);
-//        properties.put("mail.smtp.timeout", timeout);
-//        properties.put("mail.starttls.enable", starttls);
-//        properties.put("mail.smtp.ssl.enable", sslEnable);
-//        properties.put("mail.debug", debugEnable);
-//        properties.put("mail.smtp.port", port);
-////        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-////        properties.put("mail.smtp.socketFactory.class", javax.net.ssl.SSLSocketFactory.class);
-////        properties.put("mail.smtp.protocol", "smtp");
-////        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
-//
-//        return properties;
-//    }
-//}
+package com.multi.blogging.multiblogging.mail.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setJavaMailProperties(getMailProperties());
+
+        return mailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.debug", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+//        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+//        properties.put("mail.smtp.socketFactory.class", javax.net.ssl.SSLSocketFactory.class);
+//        properties.put("mail.smtp.protocol", "smtp");
+//        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        return properties;
+    }
+}
