2e5beb539aa91ffdcba8a360c3563e60de3e1a52
goldsergeant <rmagksfla000@gmail.com>
2023-10-08 18:07:18 +0900

    refactor: UpdateProfileImageRequestDto

--- src/main/java/com/multi/blogging/multiblogging/auth/dto/UpdateProfileImageRequestDto.java
+++ src/main/java/com/multi/blogging/multiblogging/auth/dto/UpdateProfileImageRequestDto.java
@@ -6,8 +6,9 @@ import org.hibernate.validator.constraints.URL;
 
 @Data
 public class UpdateProfileImageRequestDto {
+    final String invalidMessage = "URL 형식으로 입력해주세요.";
 
-    @URL(message = "URL형식으로 입력해주세요.")
-    @NotBlank(message = "URL형식으로 입력해주세요.")
+    @URL(message = invalidMessage)
+    @NotBlank(message = invalidMessage)
     public String imageUrl;
 }
