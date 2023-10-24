ff9b32715143392305db3a97986307193b6c4c91
goldsergeant <rmagksfla000@gmail.com>
2023-10-24 16:17:55 +0900

    feat: Board.java 중간작업

---
+++ src/main/java/com/multi/blogging/multiblogging/board/domain/Board.java
@@ -0,0 +1,21 @@
+package com.multi.blogging.multiblogging.board.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private Object content;
+
+    @Column(nullable = false)
+    private String thumbnailUrl;
+}
